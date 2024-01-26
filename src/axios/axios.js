01781f64dcc3e8b18be60e5cfadc6a3c68a15003
Varad Dhumale <varaddhumale177@gmail.com>
2024-01-26 16:41:26 +0530

    font size fixed

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: 'http://localhost:5000', // replace with your API base URL
-    //baseURL: 'https://bll-backend.onrender.com', 
+  // baseURL: 'http://localhost:5000', // replace with your API base URL
+    baseURL: 'https://bll-backend.onrender.com', 
  
 });
 
